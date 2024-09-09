d541a84a2d73c611a4302207c75c3150e6bff694
zhyun <jihyun.yun2@cj.net>
2024-09-09 13:02:45 +0900

    counter update

--- components/counter.tsx
+++ components/counter.tsx
@@ -3,12 +3,15 @@
 import { useState } from "react";
 import { Button } from "@nextui-org/button";
 
-export const Counter = () => {
+export const Counter: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [count, setCount] = useState(0);
 
   return (
-    <Button radius="full" onPress={() => setCount(count + 1)}>
-      Count is {count}
-    </Button>
+    <>
+      <Button radius="full" onPress={() => setCount(count + 1)}>
+        Count is {count}
+      </Button>
+      {children}
+    </>
   );
 };

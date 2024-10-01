c2e1cf3090556ebc92a09599bf1a7dbd4d80be88
이진서 <jin@ijinseoui-MacBookAir.local>
2024-10-01 23:27:39 +0900

    alter table

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,9 +1,10 @@
 import { title } from "@/components/primitives";
-import TodosTable from "@/components/todos-table"
+import TodosTable from "@/components/todos-table";
 
 async function fetchTodosApiCall(){
   console.log("fetchTodosApiCall");
   const res = await fetch(`${process.env.BASE_URL}/api/todos/`);
+  //const res = await fetch(`http://localhost:3000/api/todos/`);
 
   return res.json();
 }
@@ -11,12 +12,11 @@ async function fetchTodosApiCall(){
 export default async function TodosPage() {
   
   const response = await fetchTodosApiCall();
-
+  console.log(response);
   return (
     <div className="flex flex-col space-y-8">
       <h1 className={title()}>Todos</h1>
-      {/* <TodosTable todos= {response.data ?? []}/> */}
-      <TodosTable todos= {[]}/>
+      { <TodosTable todos= {response.data ?? []}/> }
     </div>
   );
 }
